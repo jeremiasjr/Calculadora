764d7581b8c227506b1666a12f6f6d35033df7a5
u-jejunior <u-jejunior@UHT45NB017132>
2022-07-14 17:16:18 -0300

    Adicionar arquivos de projeto.

---
+++ Calculadora/Class1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    public class Class1
+    {
+        public static void Moldura()
+        {
+            //Moldura da tela
+            int i, j;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(" ");
+            for (i = 0; i < 25; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine(" ");
+
+            for (j = 0; j < 11; j++)
+            {
+                Console.Write("|");
+                for (i = 0; i < 25; i++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("|");
+            }
+
+            Console.Write(" ");
+            for (i = 0; i < 25; i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine(" ");
+        }
+        public static void Menu()
+
+        {
+            //Menu
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.SetCursorPosition(1, 1);
+            Console.Write(" CALCULADORA DE JEREMIAS");
+
+            Console.SetCursorPosition(1, 3);
+            Console.WriteLine("Menu");
+
+            Console.SetCursorPosition(1, 4);
+            Console.WriteLine("1 - SOMA");
+
+            Console.SetCursorPosition(1, 5);
+            Console.WriteLine("2 - SUBTRAÇÃO");
+
+            Console.SetCursorPosition(1, 6);
+            Console.WriteLine("3 - MULTIPLICAÇÃO");
+
+            Console.SetCursorPosition(1, 7);
+            Console.WriteLine("4 - DIVISÃO");
+
+            Console.SetCursorPosition(1, 8);
+            Console.WriteLine("5 - SAIR");
+            Console.SetCursorPosition(1, 9);
+        }
+    }
+}
---
+++ Calculadora/Program.cs
@@ -0,0 +1,124 @@
+﻿Retorno:
+Calculadora.Class1.Moldura();
+Calculadora.Class1.Menu();
+
+int n1, n2, res;
+
+ConsoleKeyInfo cki;
+bool contLoop = true;
+while (contLoop)
+    if (Console.KeyAvailable)
+    {
+        cki = Console.ReadKey(true);
+        switch (cki.Key)
+        {
+            case ConsoleKey.NumPad1: // Operação de SOMA
+                {
+                    Console.Clear();
+                    Calculadora.Class1.Moldura();
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.SetCursorPosition(1, 1);
+                    Console.WriteLine("          SOMA          ");
+                    Console.SetCursorPosition(1, 3);
+                    Console.Write("PRIMEIRO NUMERO: ");
+                    n1 = Convert.ToInt32(Console.ReadLine());
+                    Console.SetCursorPosition(1, 5);
+                    Console.Write("SEGUNDO NUMERO: ");
+                    n2 = Convert.ToInt32(Console.ReadLine());
+
+                    res = n1 + n2;
+                    Console.SetCursorPosition(1, 7);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write($"RESULTADO = {res}");
+                    goto continua;
+                }
+            case ConsoleKey.NumPad2: // Operação de SUBTRAÇÃO
+                {
+                    Console.Clear();
+                    Calculadora.Class1.Moldura();
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.SetCursorPosition(1, 1);
+                    Console.WriteLine("        SUBTRAÇÃO       ");
+                    Console.SetCursorPosition(1, 3);
+                    Console.Write("PRIMEIRO NUMERO: ");
+                    n1 = Convert.ToInt32(Console.ReadLine());
+                    Console.SetCursorPosition(1, 5);
+                    Console.Write("SEGUNDO NUMERO: ");
+                    n2 = Convert.ToInt32(Console.ReadLine());
+
+                    res = n1 - n2;
+                    Console.SetCursorPosition(1, 7);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write($"RESULTADO = {res}");
+                    goto continua;
+                }
+            case ConsoleKey.NumPad3: // Opoeração de MULTIPLICAÇÃO
+                {
+                    Console.Clear();
+                    Calculadora.Class1.Moldura();
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.SetCursorPosition(1, 1);
+                    Console.WriteLine("      MULTIPLICAÇÃO     ");
+                    Console.SetCursorPosition(1, 3);
+                    Console.Write("PRIMEIRO NUMERO: ");
+                    n1 = Convert.ToInt32(Console.ReadLine());
+                    Console.SetCursorPosition(1, 5);
+                    Console.Write("SEGUNDO NUMERO: ");
+                    n2 = Convert.ToInt32(Console.ReadLine());
+
+                    res = n1 * n2;
+                    Console.SetCursorPosition(1, 7);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write($"RESULTADO = {res}");
+                    goto continua;
+                }
+            case ConsoleKey.NumPad4: //Operação de DIVISÃO
+                {
+                    Console.Clear();
+                    Calculadora.Class1.Moldura();
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.SetCursorPosition(1, 1);
+                    Console.WriteLine("        DIVISÃO        ");
+                    Console.SetCursorPosition(1, 3);
+                    Console.Write("PRIMEIRO NUMERO: ");
+                    n1 = Convert.ToInt32(Console.ReadLine());
+                    Console.SetCursorPosition(1, 5);
+                    Console.Write("SEGUNDO NUMERO: ");
+                    n2 = Convert.ToInt32(Console.ReadLine());
+
+                    res = n1 / n2;
+                    Console.SetCursorPosition(1, 7);
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write($"RESULTADO = {res}");
+                    goto continua;
+                }
+            case ConsoleKey.NumPad5: //SAI DO PROGRAMA
+                {
+                    Environment.Exit(0);
+                    break;
+                }
+        }
+    }
+continua:  
+Console.ForegroundColor = ConsoleColor.White;
+Console.SetCursorPosition(1, 9);
+Console.Write("Voltar ao Menu? (S/N)");
+
+//consulta de teclas nesse caso para (S/N)
+ConsoleKeyInfo cke;
+bool continuarLoop = true;
+        while (continuarLoop)
+            if (Console.KeyAvailable)
+            {
+                cke = Console.ReadKey(true);
+                switch (cke.Key)
+                {
+                    case ConsoleKey.S:
+                        Console.Clear();
+                        goto Retorno;
+                    case ConsoleKey.N:
+                        continuarLoop = false;
+                        Environment.Exit(0);
+                        break;
+                }
+            }
